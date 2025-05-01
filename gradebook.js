08d4a6beb971db16c1f79da118e172e64b2bd7f9
rumblesloth <148259673+rumblesloth@users.noreply.github.com>
2025-04-30 22:26:56 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,17 +1,41 @@
-// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
-    // This function will query the PostgreSQL database and return grade data
-    console.log("Fetching grade data...");
+  console.log("Fetching grade data...");
+  var xhr = new XMLHttpRequest();
+  var apiRoute = "/api/grades";
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        populateGradebook(JSON.parse(xhr.responseText));
+      } else {
+        console.error("Could not get grades. Status:", xhr.status);
+      }
+    }
+  };
+
+  xhr.open("GET", apiRoute, true);
+  xhr.send();
 }
 
-// TODO: Populate the table with grade data
 function populateGradebook(data) {
-    // This function will take the fetched grade data and populate the table
-    console.log("Populating gradebook with data:", data);
+  console.log("Populating gradebook with data:", data);
+  const tableElm = document.getElementById("gradebook").getElementsByTagName('tbody')[0];
+
+  data.forEach(function (assignment) {
+    const row = document.createElement("tr");
+
+    const nameCell = document.createElement("td");
+    nameCell.textContent = `${assignment.last_name}, ${assignment.first_name}`;
+    row.appendChild(nameCell);
+
+    const gradeCell = document.createElement("td");
+    gradeCell.textContent = assignment.total_grade;
+    row.appendChild(gradeCell);
+
+    tableElm.appendChild(row);
+  });
 }
 
-// TODO REMOVE THIS
-// Call the stubs to demonstrate the workflow
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
-// END REMOVE
+window.onload = function () {
+  fetchGradeData();
+};
